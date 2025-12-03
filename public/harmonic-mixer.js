3fa6762e80754b6ce36c2b0388f2387ee0228e16
Akhror-de <khabibullaevakhrorjon@gmail.com>
2025-12-03 20:10:06 +0300

    Add HarmonicMixer class for AI logic module

---
+++ public/harmonic-mixer.js
@@ -0,0 +1,15 @@
+/**
+ * Harmonic Mixer - AI Logic Module
+ */
+
+export class HarmonicMixer {
+    constructor() {
+        this.camelotWheel = CAMELOT_WHEEL;
+    }
+    
+    createSequence(tracks) {
+        // Создание гармонической последовательности
+    }
+    
+    // ... другие методы
+}
